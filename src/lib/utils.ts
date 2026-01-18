98b5251923669fba8b43f612aeb274924f609e67
Rhzslya <rhzslyaaa@gmail.com>
2026-01-18 22:44:08 +0700

    axios setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AxiosError } from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function handleApiError(error: unknown): string {
+  if (error instanceof AxiosError) {
+    if (error.response?.data?.errors) {
+      return error.response.data.errors;
+    }
+    return error.message;
+  }
+
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  return "An unknown error occurred";
 }
