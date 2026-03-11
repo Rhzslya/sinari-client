e13b4f7f587d6fd4d797c3e1eed9f0ba25fbf6d8
Rhzslya <rhzslyaaa@gmail.com>
2026-03-11 22:56:46 +0700

    upate url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { isAxiosError } from "axios";
 import { toast } from "sonner";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
   withCredentials: true,
 });
 
